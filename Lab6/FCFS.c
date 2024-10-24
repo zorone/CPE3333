c68dbf1163ed72386d64c329d3b3168ebb6f5e4d
Imron Jehleh <imron.jehleh@gmail.com>
2024-10-24 10:37:54 +0700

    Update codes to fix some indenting.

--- Lab6/FCFS.c
+++ Lab6/FCFS.c
@@ -3,39 +3,40 @@
 int main()
 {
 	char pn[10][10];
-	int arr[10],bur[10],star[10],finish[10],tat[10],wt[10],i,n;
-	int totwt=0,tottat=0;
+	int arr[10], bur[10], star[10], finish[10], tat[10], wt[10], i, n;
+	double totwt = 0, tottat = 0;
 	printf("Enter the number of processes:");
-	scanf("%d",&n);
-	for(i=0;i<n;i++)
+	scanf("%d", &n);
+	for(i = 0; i < n; i++)
 	{
 		printf("Enter the Process Name, Arrival Time & Burst Time:");
-		scanf("%s%d%d",&pn[i],&arr[i],&bur[i]);
+		scanf("%s%d%d", pn[i], &arr[i], &bur[i]);
 	}
-	for(i=0;i<n;i++)
+	for(i = 0; i < n; i++)
 	{
-		if(i==0)
+		if(i == 0)
 		{
-			star[i]=arr[i];
-			wt[i]=star[i]-arr[i];
-			finish[i]=star[i]+bur[i];
-			tat[i]=finish[i]-arr[i];
+			star[i] = arr[i];
+			wt[i] = star[i] - arr[i];
+			finish[i] = star[i] + bur[i];
+			tat[i] = finish[i] - arr[i];
 		}
 		else
 		{
-			star[i]=finish[i-1];
-			wt[i]=star[i]-arr[i];
-			finish[i]=star[i]+bur[i];
-			tat[i]=finish[i]-arr[i];
+			star[i] = finish[i-1];
+			wt[i] = star[i] - arr[i];
+			finish[i] = star[i] + bur[i];
+			tat[i] = finish[i] - arr[i];
 		}
 	}
-	printf("\nPName		Arrtime	Burtime	Start	TAT	Finish");
+	printf("\n%-8s%-8s%-8s%-8s%-8s%-8s", "PName", "Arrtime", "Burtime", "Start", "TAT", "Finish");
 	for(i=0;i<n;i++)
 	{
-		printf("\n%s\t%6d\t\t%6d\t%6d\t%6d\t%6d",pn[i],arr[i],bur[i],star[i],tat[i],finish[i]);
-	totwt+=wt[i];
-	tottat+tat[i];
+		printf("\n%-8s%8d%8d%8d%8d%8d", pn[i], arr[i], bur[i], star[i], tat[i], finish[i]);
+	totwt += wt[i];
+	tottat += tat[i];
 	}
-	printf("\nAverage Waiting time:%f", (float)totwt);
-	printf("\nAverage Turn Around Time:%f", (float)tottat);
+	printf("\nAverage Waiting time: %lf", totwt);
+	printf("\nAverage Turn Around Time: %lf", tottat);
+	return 0;
 }
